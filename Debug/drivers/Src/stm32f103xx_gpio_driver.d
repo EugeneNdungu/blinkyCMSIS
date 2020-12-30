62b263b3064737d9d2ba1b70fcf2955ae4b47c91
Eugene Mwangi <mwangieugen80@gmail.com>
2020-12-30 15:42:25 +0300

    Deletes debug folder

--- Debug/drivers/Src/stm32f103xx_gpio_driver.d
+++
@@ -1,8 +0,0 @@
-drivers/Src/stm32f103xx_gpio_driver.o: \
- ../drivers/Src/stm32f103xx_gpio_driver.c \
- C:/Users/Eugene/Documents/STM32CubeIDE/MCU1-Course/stm32f1xxx_drivers/drivers/Inc/stm32f103xx_gpio_driver.h \
- C:/Users/Eugene/Documents/STM32CubeIDE/MCU1-Course/stm32f1xxx_drivers/drivers/Inc/stm32f103xx.h
-
-C:/Users/Eugene/Documents/STM32CubeIDE/MCU1-Course/stm32f1xxx_drivers/drivers/Inc/stm32f103xx_gpio_driver.h:
-
-C:/Users/Eugene/Documents/STM32CubeIDE/MCU1-Course/stm32f1xxx_drivers/drivers/Inc/stm32f103xx.h:
